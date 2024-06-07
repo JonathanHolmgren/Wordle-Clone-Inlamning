1125713f2459ad354953ab3c098e1dbe2c71faa7
´jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-06-07 23:46:55 +0200

    update project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import WordleGame from './components/WordleGame'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1>Hello</h1>
+     <WordleGame/>
     </>
   )
 }
