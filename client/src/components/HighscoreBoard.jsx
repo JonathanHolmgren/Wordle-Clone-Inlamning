adc4c5090ca52a43263d8619564b6c2ce23f8cbe
´jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-06-08 19:45:07 +0200

    add scoreboard for Highscore

---
+++ client/src/components/HighscoreBoard.jsx
@@ -0,0 +1,21 @@
+import '../styles/HighscoreBoard.css';
+
+function HighscoreBoard() {
+  const scores = Array(10).fill({ name: 'Joe Doe', score: '1 min 2s' });
+
+  const renderScores = scores.map((item, idx) => (
+    <li key={idx}>
+      Name: {item.name} - Score: {item.score}{' '}
+    </li>
+  ));
+
+  return (
+    <div className='container'>
+      <div className='scoreboard'>
+        <ul>{renderScores}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default HighscoreBoard;
--- client/src/pages/HighscorePage.jsx
+++ client/src/pages/HighscorePage.jsx
@@ -1,10 +1,14 @@
+import '../styles/HighscorePage.css';
+
 import NavBar from '../components/NavBar';
+import HighscoreBoard from '../components/HighscoreBoard';
 
 function HighscorePage() {
   return (
     <>
       <NavBar />
-      <h1>HighScore Page</h1>
+      <h1 className='headingHighscore'>Highscores</h1>
+      <HighscoreBoard />
     </>
   );
 }
