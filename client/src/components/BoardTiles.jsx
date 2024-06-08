17de4e58aaf9a37728f5beb1e32ac22c392a2a6e
´jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-06-08 15:07:25 +0200

    add function fpr not submit, must equal five letters.

--- client/src/components/BoardTiles.jsx
+++ client/src/components/BoardTiles.jsx
@@ -1,37 +1,25 @@
-import { useState, useEffect } from "react";
-import BoardRowTiles from "./BoardRowTiles";
+import { useState, useEffect } from 'react';
+import BoardRowTiles from './BoardRowTiles';
 
-import '../styles/BoardTiles.css'
+import '../styles/BoardTiles.css';
 
+function BoardTiles({ guessedLetter, checkWin, numOfAtt, countGuesses }) {
+  const [guessWords, SetguessWords] = useState(Array(numOfAtt).fill(''));
+  const [results, Setresult] = useState(Array(numOfAtt).fill(''));
 
-function BoardTiles({ guessedLetter, checkWin, numOfAtt, countGuesses}) {
-
-  const result = [
-    ["wrong", "wrong", "wrong", "wrong-location", "wrong-location"],
-    ["correct", "correct", "wrong", "wrong-location", "wrong"],
-    ["correct", "correct", "correct", "correct", "correct"],
-    ["active", "active", "active", "active", "active"],
-    ["active", "active", "active", "active", "active"]
-]
-
-const [guessWords, SetguessWords] = useState(Array(numOfAtt).fill(""))
-const [results, Setresult] = useState(Array(numOfAtt).fill(""))
-
-useEffect(() => {
-  if (countGuesses >= 0 && countGuesses < numOfAtt) {
-    Setresult(curr => {
-      const newresults = [...curr];
-      newresults[countGuesses] = checkWin;
-      return newresults;
-    });
-  }
-}, [checkWin]);
-
- 
-
-useEffect(() => {
+  useEffect(() => {
     if (countGuesses >= 0 && countGuesses < numOfAtt) {
-        SetguessWords(curr => {
+      Setresult((curr) => {
+        const newresults = [...curr];
+        newresults[countGuesses] = checkWin;
+        return newresults;
+      });
+    }
+  }, [checkWin]);
+
+  useEffect(() => {
+    if (countGuesses >= 0 && countGuesses < numOfAtt) {
+      SetguessWords((curr) => {
         const newGuessWords = [...curr];
         newGuessWords[countGuesses] = guessedLetter;
         return newGuessWords;
@@ -39,25 +27,15 @@ useEffect(() => {
     }
   }, [guessedLetter, countGuesses, numOfAtt]);
 
-
-  const cons = () => {
-    console.log(guessWords)
-  }
-
-
-    return (
-        <div data-guess-grid className="guess-grid">
-            <BoardRowTiles word={guessWords[0]} result={results[0 + 1]} />
-            <BoardRowTiles word={guessWords[1]} result={results[1 + 1]} />
-            <BoardRowTiles word={guessWords[2]} result={results[2 + 1]} />
-            <BoardRowTiles word={guessWords[3]} result={results[3 + 1]} />
-            <BoardRowTiles word={guessWords[4]} result={results[4 + 1]} />
-            <button onClick={cons}>Click here</button>
-        </div>
-    );
+  return (
+    <div data-guess-grid className='guess-grid'>
+      <BoardRowTiles word={guessWords[0]} result={results[0 + 1]} />
+      <BoardRowTiles word={guessWords[1]} result={results[1 + 1]} />
+      <BoardRowTiles word={guessWords[2]} result={results[2 + 1]} />
+      <BoardRowTiles word={guessWords[3]} result={results[3 + 1]} />
+      <BoardRowTiles word={guessWords[4]} result={results[4 + 1]} />
+    </div>
+  );
 }
 
-
 export default BoardTiles;
-
-
--- client/src/components/WordleGame.jsx
+++ client/src/components/WordleGame.jsx
@@ -1,50 +1,53 @@
-import { useState, useEffect } from "react";
-import BoardTiles from "./BoardTiles";
-import '../styles/WordleGame.css'
-import checkIfTwoWordMatch from '../utils/checkWord'
-import Keyboard from './Keyboard'
-
-
+import { useState, useEffect } from 'react';
+import BoardTiles from './BoardTiles';
+import '../styles/WordleGame.css';
+import checkIfTwoWordMatch from '../utils/checkWord';
+import Keyboard from './Keyboard';
 
 function WordleGame() {
+  const WORD_LENGTH = 5;
+  const NUM_ATTEMPT = 6;
 
-    const WORD_LENGTH = 5;
-    const NUM_ATTEMPT = 6;
-   
+  const [guessedLetters, setGuessedLetters] = useState('');
+  const [checkWin, setCheckWin] = useState();
+  const [countGuesses, setCounterGuesses] = useState(0);
 
-    const [guessedLetters, setGuessedLetters] = useState("");
-    const [checkWin, setCheckWin] = useState();
-    const [countGuesses, setCounterGuesses] = useState(0);
-
-
-    const submitGuess = () => {
-        if (countGuesses <= NUM_ATTEMPT) {
-           
-            console.log(checkIfTwoWordMatch("hello", guessedLetters.join("")))
-            setCheckWin(checkIfTwoWordMatch("hello", guessedLetters.join("")))
-            setCounterGuesses(countGuesses + 1)
-            setGuessedLetters("");
-        }
+  const submitGuess = () => {
+    if (countGuesses <= NUM_ATTEMPT && guessedLetters.length == 5) {
+      console.log(checkIfTwoWordMatch('hello', guessedLetters.join('')));
+      setCheckWin(checkIfTwoWordMatch('hello', guessedLetters.join('')));
+      setCounterGuesses(countGuesses + 1);
+      setGuessedLetters('');
     }
+  };
 
-    function handleKeyPress(key) {
-        if (guessedLetters.length >= WORD_LENGTH) return;
-        setGuessedLetters([...guessedLetters, key]);
+  function handleKeyPress(key) {
+    if (guessedLetters.length >= WORD_LENGTH) return;
+    setGuessedLetters([...guessedLetters, key]);
+  }
+
+  function removeLetter() {
+    if (guessedLetters.length > 0) {
+      setGuessedLetters(guessedLetters.slice(0, -1));
     }
+  }
 
-    function removeLetter() {
-        if (guessedLetters.length > 0) {
-            setGuessedLetters(guessedLetters.slice(0, -1))
-        }
-    }
-
-    return (
-        <>
-            <BoardTiles guessedLetter={guessedLetters} checkWin={checkWin} numOfAtt={NUM_ATTEMPT} countGuesses={countGuesses}  />
-            <Keyboard onKeyClick={handleKeyPress} submitGuess={submitGuess} removeLetter={removeLetter} />
-        </>
-
-    )
+  return (
+    <>
+      <button>Reset game</button>
+      <BoardTiles
+        guessedLetter={guessedLetters}
+        checkWin={checkWin}
+        numOfAtt={NUM_ATTEMPT}
+        countGuesses={countGuesses}
+      />
+      <Keyboard
+        onKeyClick={handleKeyPress}
+        submitGuess={submitGuess}
+        removeLetter={removeLetter}
+      />
+    </>
+  );
 }
 
 export default WordleGame;
