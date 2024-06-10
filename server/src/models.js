bfc0ada4b4c635abbcc479f758d0cc5351a548dc
´jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-06-10 17:35:08 +0200

    change schema structure

--- server/src/models.js
+++ server/src/models.js
@@ -3,6 +3,9 @@ import { mongoose } from 'mongoose';
 const Highscore = mongoose.model('Highscore', {
   username: String,
   time: String,
+  countGuesses: Number,
+  guessWords: Array,
+  wordLength: Number,
 });
 
 export { Highscore };
